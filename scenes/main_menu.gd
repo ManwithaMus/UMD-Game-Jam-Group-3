14c461eee2d62b1303798467fe07f4e66e482d64
Shadooba <sickman2006@gmail.com>
2025-11-19 20:47:31 -0500

    Main Menu
    Added option menu and quit functionality

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -3,7 +3,9 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$OptionsPopup.visible = false
+	AudioServer.set_bus_volume_db(0,linear_to_db(15))
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,3 +15,38 @@ func _process(delta: float) -> void:
 
 func start_button_pressed() -> void:
 		get_tree().change_scene_to_file("res://scenes/levels/level_1.tscn")
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_options_pressed() -> void:
+	$OptionsPopup.visible = true
+
+
+func _on_volume_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(0,linear_to_db(value))
+
+
+func _on_resolution_item_selected(index: int) -> void:
+	match index:
+		0:
+			DisplayServer.window_set_size(Vector2i(1920,1080))
+		1:
+			DisplayServer.window_set_size(Vector2i(1280,720))
+		2:
+			DisplayServer.window_set_size(Vector2i(1152,648))
+		3:
+			DisplayServer.window_set_size(Vector2i(800,600))
+
+
+func _on_full_screen_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+
+func _on_close_pressed() -> void:
+	$OptionsPopup.visible = false
